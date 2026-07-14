3c5fa0b850da62834736d50af12459dff65639c2
Brian Buvinghausen <buvinghausen@users.noreply.github.com>
2026-07-14 01:21:08 -0400

    Asgard mediator fixes

---
+++ tests/Abstractions.Web.Server.Tests/Mediator/IRequestHandlerTests.cs
@@ -0,0 +1,24 @@
+using Norse.Abstractions.Web.Server.Mediator;
+
+namespace Norse.Abstractions.Web.Server.Tests.Mediator;
+
+public sealed class IRequestHandlerTests
+{
+	[Fact]
+	async Task Handle_works_for_a_request_type_that_implements_nothing()
+	{
+		IRequestHandler<UnconstrainedRequest, bool> handler = new EchoHandler();
+
+		var result = await handler.Handle(new UnconstrainedRequest(), CancellationToken.None);
+
+		result.ShouldBeTrue();
+	}
+
+	sealed record UnconstrainedRequest;
+
+	sealed class EchoHandler : IRequestHandler<UnconstrainedRequest, bool>
+	{
+		public ValueTask<bool> Handle(UnconstrainedRequest request, CancellationToken cancellationToken) =>
+			ValueTask.FromResult(true);
+	}
+}
